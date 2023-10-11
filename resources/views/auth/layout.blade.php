6b702918a35a76f0d887c29da5be99e8dc313f82
ludovic <developer.ludovic@gmail.com>
2023-10-11 14:15:54 +0300

    add login page

---
+++ resources/views/auth/layout.blade.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}" class="h-100">
+<head>
+    @include('layouts.partials._meta')
+	<title>Hospital Management System</title>
+    <link rel="icon" type="image/png" sizes="16x16" href="{{asset('assets/images/mini.png')}}" />
+    <link href="{{asset('assets/css/style.css')}}" rel="stylesheet" />
+    <link
+      href="https://fonts.googleapis.com/css2?family=Poppins:wght@100;200;300;400;500;600;700;800;900&amp;family=Roboto:wght@100;300;400;500;700;900&amp;display=swap"
+      rel="stylesheet" />
+</head>
+
+<body class="h-100">
+    <div class="authincation h-100">
+        <div class="container h-100">
+            <div class="row justify-content-center h-100 align-items-center">
+                <div class="col-md-6">
+                    <div class="authincation-content">
+                        <div class="row no-gutters">
+                            <div class="col-xl-12">
+                                <div class="auth-form">
+									<div class="text-center mb-3">
+										<a href="index.html"><img src="{{asset('assets/images/mini.png')}}" style="max-height: 55px; max-width: 55px;" alt="logo"></a>
+									</div>
+                                    <h4 class="text-center mb-4 text-white">@yield('heading')</h4>
+                                    <form action="@yield('formRoute')" method="POST">
+                                        @csrf
+                                        @yield('content')
+                                    </form>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</body>
+</html>
--- resources/views/auth/passwords/confirm.blade.php
+++ resources/views/auth/passwords/confirm.blade.php
@@ -1,48 +1,27 @@
-@extends('layouts._master')
+@extends('auth.layout')
+@section('heading','Password confirm')
+@section('formRoute')
+    {{ route('password.confirm') }}
+@endsection
 @section('content')
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">{{ __('Confirm Password') }}</div>
-
-                <div class="card-body">
-                    {{ __('Please confirm your password before continuing.') }}
-
-                    <form method="POST" action="{{ route('password.confirm') }}">
-                        @csrf
-
-                        <div class="row mb-3">
-                            <label for="password" class="col-md-4 col-form-label text-md-end">{{ __('Password') }}</label>
-
-                            <div class="col-md-6">
-                                <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="current-password">
-
-                                @error('password')
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
-                                @enderror
-                            </div>
-                        </div>
-
-                        <div class="row mb-0">
-                            <div class="col-md-8 offset-md-4">
-                                <button type="submit" class="btn btn-primary">
-                                    {{ __('Confirm Password') }}
-                                </button>
-
-                                @if (Route::has('password.request'))
-                                    <a class="btn btn-link" href="{{ route('password.request') }}">
-                                        {{ __('Forgot Your Password?') }}
-                                    </a>
-                                @endif
-                            </div>
-                        </div>
-                    </form>
-                </div>
-            </div>
-        </div>
+<div class="form-group">
+    <label class="mb-1 text-white"><strong>Password</strong></label>
+    <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="current-password">
+    @error('password')
+        <span class="invalid-feedback" role="alert">
+            <strong>{{ $message }}</strong>
+        </span>
+    @enderror
+</div>
+<div class="row d-flex justify-content-srart mt-4 mb-2">
+    @if (Route::has('password.request'))
+    <div class="form-group">
+        <a class="text-white" href="{{ route('password.request') }}">Forgot Password?</a>
     </div>
+    @endif
+</div>
+<div class="text-center">
+    <button type="submit" class="btn bg-white text-primary btn-block">Confirm</button>
 </div>
 @endsection
+
